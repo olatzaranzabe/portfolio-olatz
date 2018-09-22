22187ef9bdaf13e8b616762d8c39d69c7656d428
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-23 00:21:41 +0200

    install react-router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Header from './Header';
+import Main from './Main';
+import Curriculum from './Curriculum';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Main />
+        <Curriculum />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/Components/Curriculum.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Curriculum extends Component {
+ 
+    render() { 
+        return ( 
+            <div className='cv-olatz'> My cv</div>
+         );
+    }
+}
+ 
+export default Curriculum;
