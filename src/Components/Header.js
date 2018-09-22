7ee20b9f9ed0ec9b512926190c989b95c9119395
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-22 23:57:00 +0200

    create header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+ 
+    render() { 
+        return ( 
+            <div className='header'> Olatz</div>
+         );
+    }
+}
+ 
+export default Header;
